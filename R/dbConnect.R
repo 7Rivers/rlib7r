5b5f495fed6c9f530432e47795d6e7f0b71f91c6
Fah Sathirapongsasuti <fah.s@medgenome.com>
2020-02-13 05:01:36 +0530

    first commit; add package skeleton

---
+++ R/dbConnect.R
@@ -0,0 +1,11 @@
+#' Connect to the SQL database
+#'
+#' @param db.info A list with fields: user, password, dbname, and host 
+# @export
+dbConnect = function(db.info) {
+    require(RMySQL) 
+    con = dbConnect(MySQL(), user=db.info$user, password=db.info$password,
+                    dbname=db.info$dbname, host=db.info$host) 
+    return(con)
+}
+
