253b24ef6f06f566d8784e4669719ad3a8617e81
fahs-mg <60985799+fahs-mg@users.noreply.github.com>
2020-02-12 19:08:24 -0800

    Minor fix, M-->m

--- R/myDbConnect.R
+++ R/myDbConnect.R
@@ -2,7 +2,7 @@
 #'
 #' @param db.info A list with fields: user, password, dbname, and host 
 #' @export
-MyDbConnect = function(db.info) {
+myDbConnect = function(db.info) {
     con = dbConnect(MySQL(), user=db.info$user, password=db.info$password,
                     dbname=db.info$dbname, host=db.info$host) 
     return(con)
