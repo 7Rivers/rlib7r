5961eac420268a37d6ce85389c1d258c3a23f40d
fahs-mg <60985799+fahs-mg@users.noreply.github.com>
2020-02-12 19:06:27 -0800

    Make MyDbConnect and with .dbConnect as internal

--- R/myDbConnect.R
+++ R/myDbConnect.R
@@ -2,9 +2,10 @@
 #'
 #' @param db.info A list with fields: user, password, dbname, and host 
 #' @export
-.dbConnect = function(db.info) {
+MyDbConnect = function(db.info) {
     con = dbConnect(MySQL(), user=db.info$user, password=db.info$password,
                     dbname=db.info$dbname, host=db.info$host) 
     return(con)
 }
 
+.dbConnect <- function(...) { myDbConnect(...) }
