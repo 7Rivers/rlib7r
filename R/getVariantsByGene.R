963eac30b9b11d08b36de33b61245932f9c5f175
fahs-mg <60985799+fahs-mg@users.noreply.github.com>
2020-02-12 19:02:18 -0800

    Update .dbConnect and example

--- R/getVariantsByGene.R
+++ R/getVariantsByGene.R
@@ -4,12 +4,12 @@
 #' @param vep_var_impact One of {HIGH, LOW, MODERATE, MODIFIER, NA}
 #' @param canonical If true, limit to canonical transcipts
 #' @examples
-#' get.variants.by.gene(gene='PCSK9', vep_var_impact='HIGH', canonical=TRUE)
+#' getVariantsByGene(gene='PCSK9', vep_var_impact='HIGH', canonical=TRUE)
 #' @export
 getVariantsByGene = function(gene, vep_var_impact=NULL, canonical=TRUE, 
                                 con=NULL, db.info) {
     if (is.null(con)) {
-        con = dbConnect(db.info=db.info)
+        con = .dbConnect(db.info=db.info)
         on.exit(dbDisconnect(con))
     }
     sql = sprintf("select * from gene_annotation where gene_name = '%s'", gene)
