83209279d27c13d716533e4dd038ccad69632267
fahs-mg <60985799+fahs-mg@users.noreply.github.com>
2020-02-12 19:09:57 -0800

    Add RMySQL::

--- R/myDbConnect.R
+++ R/myDbConnect.R
@@ -3,7 +3,7 @@
 #' @param db.info A list with fields: user, password, dbname, and host 
 #' @export
 myDbConnect = function(db.info) {
-    con = dbConnect(MySQL(), user=db.info$user, password=db.info$password,
+    con = RMySQL::dbConnect(RMySQL::MySQL(), user=db.info$user, password=db.info$password,
                     dbname=db.info$dbname, host=db.info$host) 
     return(con)
 }
