cd16ec6c636a7859d6727653b72905272ca747c5
fahs-mg <60985799+fahs-mg@users.noreply.github.com>
2020-02-12 18:59:45 -0800

    Make it a private function

--- R/.dbConnect.R
+++ R/.dbConnect.R
@@ -2,7 +2,7 @@
 #'
 #' @param db.info A list with fields: user, password, dbname, and host 
 #' @export
-dbConnect = function(db.info) {
+.dbConnect = function(db.info) {
     con = dbConnect(MySQL(), user=db.info$user, password=db.info$password,
                     dbname=db.info$dbname, host=db.info$host) 
     return(con)
